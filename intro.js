6d750121e49ec419fb711530e03870e6d13781d1
Sourav <srv.sss40@gmail.com>
2026-05-05 23:53:57 -0400

    first commit

---
+++ intro.js
@@ -0,0 +1,58 @@
+// COMMENTS
+
+// Inline Comment
+
+/*
+Multi
+Line
+Comment
+*/
+
+
+/* DATA TYPES
+undefined = something that is not defined yet (this is different from null), 
+null, 
+boolean, 
+string, 
+symbol = creates a unique value for the var, 
+number = stores decimals, has a max limit −(2^53−1) ≤ n ≤ 2^53−1, 
+bigint = stores only int, for huge ints,
+object = python dict
+ */
+
+
+// Variable Declare
+var a;
+
+// Variable Declare and Assignment in same line
+var b = 2;
+
+console.log(a)
+
+// Variable Assignment
+a = 7;
+b = a;
+
+console.log(a)
+
+// Incrementing Numbers
+a++;
+
+console.log(a)
+
+// Decrementing Numbers
+a--
+
+console.log(a)
+
+// Self divide/multiple/add/sub etc
+// a+=, a-=, a*=, a/=
+
+
+// Escape string
+var myStr = "Hello, thats such a \"Cliche!\""
+console.log(myStr)
+
+// Length of the string
+var name = "Ada";
+console.log(name.length)
