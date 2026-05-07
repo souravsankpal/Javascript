56d36c61a5fd825f6639c9feac9a03ee236f7852
Sourav <srv.sss40@gmail.com>
2026-05-06 22:43:13 -0400

    update intro

--- intro.js
+++ intro.js
@@ -1,4 +1,4 @@
-// COMMENTS
+//1. COMMENTS
 
 // Inline Comment
 
@@ -21,7 +21,8 @@ object = python dict
  */
 
 
-// Variable Declare
+//2. Variables
+//Variable Declare
 var a;
 
 // Variable Declare and Assignment in same line
@@ -49,6 +50,7 @@ console.log(a)
 // a+=, a-=, a*=, a/=
 
 
+//3. Strings
 // Escape string
 var myStr = "Hello, thats such a \"Cliche!\""
 console.log(myStr)
@@ -56,3 +58,114 @@ console.log(myStr)
 // Length of the string
 var name = "Ada";
 console.log(name.length)
+
+
+
+//4. Arrays
+var myArray = ["Hello", 2, true, [1, 2, 3]];
+// Array can have different data types
+
+// Accessing Array Elements
+console.log(myArray[0]);
+console.log(myArray[3][0]);
+
+// Modifying Array Elements
+myArray[1] = 3;
+console.log(myArray);
+
+// Append to Array
+myArray.push(1);
+console.log(myArray);
+// Shift adds element to the begining of the array
+
+// Pop removes last element shift removes first element
+
+
+
+//5. Functions
+function addition(a, b) {
+    console.log(a + b);
+}
+
+addition(1, 5);
+
+
+// Var scopes
+
+var myGlobal = 10;
+console.log(typeof myGlobal)
+
+function func1() {
+    // oopsGlobal is not declared in the function, so it is global
+    // This is a bad practice and will not work in strict mode
+    oopsGlobal = 5;
+}
+
+
+function func2() {
+    var output = ""
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+
+    console.log(output);
+
+    return null;
+}
+
+func1();
+func2();
+
+
+//6. Operators
+
+//Strict Equality Operator
+console.log(3 == "3")
+console.log(3 === "3")
+
+//Strict Inequality Operator
+console.log(3 != "3")
+console.log(3 !== "3")
+
+//And operator &&
+
+//Or Operator ||
+
+//7. Objects
+
+var myObj = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+console.log(myObj.name);
+console.log(myObj["age"]);
+// Dot or bracket notation can be used to access properties of an object, but if the key has a space, then bracket notation must be used
+
+// Add a new property to an object
+myObj["hairColor"] = "brown";
+
+// Delete a property from an object
+delete ["hairColor"];
+
+// Check if a property exists in an object
+console.log(myObj.hasOwnProperty("hairColor"));
+
+//8. Loops
+
+// For Loop
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// While Loop
+var j = 0;
+while (j < 6) {
+    console.log(j);
+    j += 2
+}
